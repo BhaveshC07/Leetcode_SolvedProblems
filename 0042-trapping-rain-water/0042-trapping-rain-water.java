9ae589ccec925c5b4421b63374996bd7e005b8cd
Bhavesh Charde <115037516+BhaveshC07@users.noreply.github.com>
2024-03-14 23:43:51 +0530

    Time: 1 ms (71.66%), Space: 46.1 MB (51.21%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -8,17 +8,16 @@ class Solution {
         LM[0]=num[0];
         for (int i = 1; i < num.length; i++) {
             LM[i]=Math.max(num[i],LM[i-1]);
-            System.out.println(i+" "+LM [i]);
+            //System.out.println(i+" "+LM [i]);
         }
         RM[num.length-1]=num[num.length-1];
         for (int i = num.length-2; i >= 0; i--) {
             RM[i]=Math.max(num[i],RM[i+1] );
-            System.out.println(i+" "+RM[i]);
+            //System.out.println(i+" "+RM[i]);
         }
         int trappwater =0;
         for (int i = 0; i < num.length ; i++) {
             int waterlvl=Math.min(LM[i],RM[i]);
-
             trappwater+=waterlvl-num[i];
         }
         return trappwater;
