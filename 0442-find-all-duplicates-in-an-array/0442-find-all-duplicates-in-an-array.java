a2325e1e1e775c06352c799d33b0c8b97bd0807b
Bhavesh Charde <115037516+BhaveshC07@users.noreply.github.com>
2024-07-17 18:00:09 +0530

    Time: 17 ms (28.62%), Space: 53.4 MB (70.37%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -6,6 +6,7 @@ class Solution {
         for(int i=0;i<n-1;i++){
             if(nums[i]==nums[i+1]){
                 list.add(nums[i]);
+                i++;
             }
         }
         return list;
